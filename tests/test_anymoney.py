861fe0a5b537333896a09a37a190d186cd1fc72a
4yach <a.4yach@gmail.com>
2022-04-02 14:28:06 +0300

    Fixed  method, now it can create offer, but this offer is multipay.

--- tests/test_anymoney.py
+++ tests/test_anymoney.py
@@ -2,14 +2,25 @@
 from os import environ
 from tgam import AnyMoney
 from unittest import IsolatedAsyncioTestCase
+from configparser import ConfigParser
+
+
+_config = ConfigParser()
 
 
 class Test_AnyMoney(IsolatedAsyncioTestCase):
 
     def setUp(self) -> None:
-        self._any_money = AnyMoney(
-            environ.get("TOKEN"),
-            environ.get("MERCH"))
+        _config.read("config.cfg")
+
+        _test = _config["Tests"]
+
+        if _test != None:
+            self._any_money = AnyMoney(
+                _test.get("TOKEN"),
+                _test.get("MERCH"))  # renamed env. variables, to not shadow real variables
+        else:
+            raise Exception("'Tests' section is missing")
 
     async def test_call_method(self):
         data = await self._any_money.call_method(
@@ -19,7 +30,6 @@ class Test_AnyMoney(IsolatedAsyncioTestCase):
         self.assertTrue("error" not in data)
 
     async def test_create_invoice(self):
-        # Test failed: specify payway, when preform one way (single) pay
         data = await self._any_money.invoice_create(
             "5450", "USDT", "1", "a.4yach@gmail.com", "1d")
         self.assertTrue("error" not in data)
